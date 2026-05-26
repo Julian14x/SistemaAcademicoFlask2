20e70180c421be15316bc1877d73081e2b1c759f
Julian Bareño <julianbareno74@gmail.com>
2026-05-26 18:58:48 -0500

    Actualizando proyecto

--- firebase_config.py
+++ firebase_config.py
@@ -1,7 +1,10 @@
+import os
+import json
 import firebase_admin
-from firebase_admin import credentials, firestore
+from firebase_admin import credentials
 
-cred = credentials.Certificate("serviceAccountKey.json")
+# Carga las credenciales desde la variable de entorno de Vercel
+cred_json = json.loads(os.environ.get('FIREBASE_CREDENCIAL'))
+cred = credentials.Certificate(cred_json)
 firebase_admin.initialize_app(cred)
 
-db = firestore.client()
